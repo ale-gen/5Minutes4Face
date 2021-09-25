2e06f08b5f257062e44ab0db7819fc6a5967e8ca
Aleksandra Generowicz <a.generowicz@wp.pl>
2021-09-25 09:41:50 +0200

    Initial Commit

---
+++ 5Minutes4Face/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5Minutes4Face
+//
+//  Created by Aleksandra Generowicz on 25/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
